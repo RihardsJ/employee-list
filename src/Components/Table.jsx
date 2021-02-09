10b693247ffb049793996e520423adb67d532bb8
RihardsJ <rihards.jukna@gmail.com>
2021-02-09 20:38:12 +0000

    add styling to table

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -5,25 +5,83 @@ import deleteRecord from "../Utils/DeleteRecord";
 import { ReactComponent as EditSVG } from "../Images/edit-icon.svg";
 import { ReactComponent as DeleteSVG } from "../Images/delete-icon.svg";
 import { ReactComponent as ProfileSVG } from "../Images/profile-icon.svg";
+import device from "../Utils/Device-Size";
 
-const Table = styled.table``;
+const Table = styled.table`
+  width: 80%;
+  margin: 1rem auto;
+  padding: 24px;
+  box-shadow: 1px 1px 2px hsla(0, 0%, 0%, 0.4);
+  border-bottom: 1px solid var(--grey);
+  border-collapse: collapse;
+`;
 
-const THead = styled.thead``;
+const THead = styled.thead`
+  color: var(--white);
+  background-color: var(--green);
+  text-transform: capitalize;
+  font-family: var(--title-font);
+  display: none;
 
-const TBody = styled.tbody``;
+  @media ${device.samllLaptop} {
+    display: table-header-group;
+  }
+`;
+
+const TBody = styled.tbody`
+  text-align: left;
+
+  @media ${device.samllLaptop} {
+    text-align: center;
+  }
+`;
 
 const TH = styled.th``;
 
-const TD = styled.td``;
+const TD = styled.td`
+  color: ${(props) => (props.colour ? `var(--${props.colour})` : "")};
+  font-weight: ${(props) => props.fontWeight};
+  font-size: ${(props) => props.size};
+  grid-area: ${(props) => props["gridposition"]};
+`;
 
-const TR = styled.tr``;
+const TR = styled.tr`
+  display: grid;
+  padding: 1rem;
+  grid-template: 1fr 1fr 1fr / 85% 15%;
+  border-bottom: 1px solid var(--grey);
 
-const EditIcon = styled(EditSVG)``;
-const DeleteIcon = styled(DeleteSVG)``;
+  @media ${device.samllLaptop} {
+    display: table-row;
+    font-size: 1.2rem;
+  }
+`;
 
-const Button = styled.button``;
+const EditIcon = styled(EditSVG)`
+  cursor: pointer;
+  width: 15px;
+  height: auto;
+`;
+const DeleteIcon = styled(DeleteSVG)`
+  cursor: pointer;
+  width: 15px;
+  height: auto;
+`;
 
-const ProfileIcon = styled(ProfileSVG)``;
+const Button = styled.button`
+  padding: 0;
+  border-style: none;
+  background-color: transparent;
+  &:focus {
+    outline: dashed var(--red);
+  }
+`;
+
+const ProfileIcon = styled(ProfileSVG)`
+  width: 15px;
+  margin-right: 1ch;
+  height: auto;
+`;
 
 const Head = () => {
   const DisplayTitles = () => {
@@ -57,12 +115,22 @@ const Body = () => {
       rows.push(
         <TR key={i}>
           <TD>
-            <ProfileIcon />
+            <ProfileIcon gridposition="1 / 1" />
           </TD>
-          <TD>{employee.name}</TD>
-          <TD>{employee.email}</TD>
-          <TD>
+          <TD
+            gridposition="2 / 1"
+            colour="green"
+            fontWeight="700"
+            size="1.1rem"
+          >
+            {employee.name}
+          </TD>
+          <TD gridposition="3 / 1" fontWeight="400" size="1rem">
+            {employee.email}
+          </TD>
+          <TD gridposition="1 / 2">
             <Button
+              aria-label={`update-${employee.name.split(" ").join("-")}`}
               onClick={() =>
                 updateRecords(
                   employee,
@@ -74,6 +142,7 @@ const Body = () => {
               <EditIcon />
             </Button>
             <Button
+              aria-label={`delete-${employee.name.split(" ").join("-")}`}
               onClick={() => setEmployees(deleteRecord(employees, employee))}
             >
               <DeleteIcon />
