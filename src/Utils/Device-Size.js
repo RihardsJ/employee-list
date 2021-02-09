255731f0df89183e1949024f0ad9ba9ec279af96
RihardsJ <rihards.jukna@gmail.com>
2021-02-09 20:19:31 +0000

    import device-size object for media queries

---
+++ src/Utils/Device-Size.js
@@ -0,0 +1,9 @@
+const devices = {
+  largeMobile: "(min-width: 387px)",
+  smallTablet: "(min-width: 570px)",
+  tablet: "(min-width: 768px)",
+  samllLaptop: "(min-width: 1024px)",
+  laptop: "(min-width: 1440px)",
+};
+
+export default devices;
