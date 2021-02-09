a733bb205877369d477c23c0280779f2641f73cb
RihardsJ <rihards.jukna@gmail.com>
2021-02-09 16:02:26 +0000

    add input form elements

---
+++ src/Components/Button.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  cursor: pointer;
+  font-size: 1em;
+  height: 2rem;
+  padding: 0.25em 1em;
+  border-radius: 15px;
+  border-style: none;
+`;
+
+export default Button;
--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,7 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import styled from "styled-components";
+import Button from "./Button";
+import { EmployeeContext } from "../Utils/Context";
+
+const Form = styled.form``;
+
+const FieldSet = styled.fieldset``;
+
+const Legend = styled.legend``;
+
+const InputLabel = styled.label``;
+
+const Input = styled.input``;
+
+const OpenFormButton = styled(Button)``;
+
+const SubmitButton = styled(Button)``;
 
 const InputField = () => {
-  return <>Employee List</>;
+  const { employees } = useContext(EmployeeContext);
+
+  return (
+    <Form position="open" id="inputForm">
+      <OpenFormButton type="button">close</OpenFormButton>
+      <FieldSet>
+        <Legend>New Employee</Legend>
+        <InputLabel htmlFor="input-name">Name:</InputLabel>
+        <Input type="text" id="input-name" autoComplete="false" required />
+        <InputLabel htmlFor="input-email">Email:</InputLabel>
+        <Input type="text" id="input-email" autoComplete="false" />
+        <SubmitButton type="submit">Add</SubmitButton>
+      </FieldSet>
+    </Form>
+  );
 };
 
 export default InputField;
