efad1a08849e29282fb6a90392984ebd0f975120
RihardsJ <rihards.jukna@gmail.com>
2021-02-09 17:55:29 +0000

    add mockdata for testing

---
+++ src/Tests/MockData.js
@@ -0,0 +1,138 @@
+export const firstNamesOnly = [
+  {
+    name: "Mose",
+    email: "elliott23@crona.info",
+  },
+  {
+    name: "Jason",
+    email: "lemke.hayden@yahoo.com",
+  },
+  {
+    name: "Coralie",
+    email: "ycruickshank@yahoo.com",
+  },
+  {
+    name: "Lilliana",
+    email: "jaskolski.friedrich@hintz.biz",
+  },
+  {
+    name: "Willa",
+    email: "ihaag@hirthe.info",
+  },
+  {
+    name: "Blaise",
+    email: "wyman.alice@gmail.com",
+  },
+  {
+    name: "Tierra",
+    email: "ledner.greg@wunsch.com",
+  },
+  {
+    name: "Jefferey",
+    email: "shanahan.ned@yost.com",
+  },
+  {
+    name: "Jennie",
+    email: "alexandro38@gmail.com",
+  },
+];
+
+export const fullNames = [
+  {
+    name: "Osvaldo Brown",
+    email: "lhyatt@hotmail.com",
+  },
+  {
+    name: "Carleton Hessel",
+    email: "aufderhar.brody@hotmail.com",
+  },
+  {
+    name: "Tyrese Gottlieb",
+    email: "nader.braeden@yahoo.com",
+  },
+  {
+    name: "Queenie Gerlach",
+    email: "jamel27@hotmail.com",
+  },
+  {
+    name: "Loraine Walsh",
+    email: "knienow@dooley.com",
+  },
+  {
+    name: "Lupe Macejkovic PhD",
+    email: "price.rosalind@jaskolski.org",
+  },
+  {
+    name: "Chelsey Frami",
+    email: "mkemmer@haag.net",
+  },
+  {
+    name: "Judson Reilly",
+    email: "wilford.koelpin@yahoo.com",
+  },
+  {
+    name: "Vivian Schinner",
+    email: "rebeca84@yahoo.com",
+  },
+];
+
+export const closelyMatchedNames = [
+  {
+    name: "Osvaldo Brown Junior",
+    email: "lhyatt@hotmail.com",
+  },
+  {
+    name: "Loraine Walsh",
+    email: "knienow@dooley.com",
+  },
+
+  {
+    name: "Osvaldo Brown",
+    email: "lhyatt@hotmail.com",
+  },
+
+  {
+    name: "Oscar Brown",
+    email: "lhyatt@hotmail.com",
+  },
+
+  {
+    name: "Oscar",
+    email: "lhyatt@hotmail.com",
+  },
+
+  {
+    name: "Loraine Smith",
+    email: "knienow@dooley.com",
+  },
+];
+
+export const closelyMatchResults = [
+  {
+    name: "Loraine Smith",
+    email: "knienow@dooley.com",
+  },
+
+  {
+    name: "Loraine Walsh",
+    email: "knienow@dooley.com",
+  },
+
+  {
+    name: "Oscar",
+    email: "lhyatt@hotmail.com",
+  },
+  {
+    name: "Oscar Brown",
+    email: "lhyatt@hotmail.com",
+  },
+  {
+    name: "Osvaldo Brown",
+    email: "lhyatt@hotmail.com",
+  },
+
+  {
+    name: "Osvaldo Brown Junior",
+    email: "lhyatt@hotmail.com",
+  },
+];
