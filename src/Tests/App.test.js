906881f94ce23496ab8107fa4fd7cb46698ce790
RihardsJ <rihards.jukna@gmail.com>
2021-02-09 14:51:43 +0000

    add app.tests to watch essential components

--- src/Tests/App.test.js
+++ src/Tests/App.test.js
@@ -1,8 +1,35 @@
 import { render, screen } from "@testing-library/react";
 import App from "../App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText("Component here");
-  expect(linkElement).toBeInTheDocument();
+describe("DOM test", () => {
+  test("renders title", () => {
+    render(<App />);
+    const title = screen.getByText("Employee List");
+    expect(title).toBeInTheDocument();
+  });
+
+  test("renders sort select", () => {
+    render(<App />);
+    const sortSelect = screen.getByText("Sort:");
+    const option = screen.getByRole("option", { name: /A - Z/i });
+    expect(option).toBeInTheDocument();
+    expect(sortSelect).toBeInTheDocument();
+  });
+
+  test("renders table", () => {
+    render(<App />);
+    const table = screen.getByRole("table");
+    expect(table).toBeInTheDocument();
+  });
+
+  test("renders input field", () => {
+    render(<App />);
+    const field = screen.getByRole("group", { name: /New Employee/i });
+    const textboxName = screen.getByRole("textbox", { name: /Name:/i });
+    const button = screen.getByRole("button", { name: /Add/i });
+    console.log(field);
+    expect(field).toBeInTheDocument();
+    expect(textboxName).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+  });
 });
