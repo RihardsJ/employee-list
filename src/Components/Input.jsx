c3be6fe4c11f95f258766f3987a68260edd01d10
RihardsJ <rihards.jukna@gmail.com>
2021-02-09 16:06:23 +0000

    add input form validation

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -20,15 +20,29 @@ const SubmitButton = styled(Button)``;
 const InputField = () => {
   const { employees } = useContext(EmployeeContext);
 
+  const nameValidation = "[a-zA-Z ]+";
+  const emailValidation = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,4}$";
+
   return (
     <Form position="open" id="inputForm">
       <OpenFormButton type="button">close</OpenFormButton>
       <FieldSet>
         <Legend>New Employee</Legend>
         <InputLabel htmlFor="input-name">Name:</InputLabel>
-        <Input type="text" id="input-name" autoComplete="false" required />
+        <Input
+          type="text"
+          id="input-name"
+          autoComplete="false"
+          pattern={nameValidation}
+          required
+        />
         <InputLabel htmlFor="input-email">Email:</InputLabel>
-        <Input type="text" id="input-email" autoComplete="false" />
+        <Input
+          type="text"
+          id="input-email"
+          pattern={emailValidation}
+          autoComplete="false"
+        />
         <SubmitButton type="submit">Add</SubmitButton>
       </FieldSet>
     </Form>
